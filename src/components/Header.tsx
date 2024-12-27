edee8e6913369547dc701a390d1c555aa0bd689c
Jordi Roca <jordi.roca@acaitravel.com>
2024-12-27 13:48:22 +0100

    fix: header bg issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import Logo from "../icons/logo";
-import { useEffect, useState } from "react";
 
 const Header = ({ parallax }: { parallax?: boolean }) => {
   const links = [
@@ -9,25 +8,9 @@ const Header = ({ parallax }: { parallax?: boolean }) => {
     { link: "/reserve", label: "Tickets" },
   ];
 
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  const handleScroll = () => {
-    setScrollPosition(window.scrollY);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-  console.log(scrollPosition, "scrollin");
   return (
     <header
       className={`${
-        scrollPosition > 0 ? "bg-blackHeader/70 text-white" : "text-black"
-      } ${
         parallax ? "absolute top-0" : "fixed"
       } z-50 flex justify-between items-center w-full py-5 px-10 bg-transparent`}
     >
