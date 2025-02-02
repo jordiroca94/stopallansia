2bb1fb80357e5a25dd1ef99dc9d604fab47f59fe
Jordi Roca <jordi.roca@acaitravel.com>
2025-02-02 11:07:04 +0100

    fix:event

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,5 +3,24 @@ import { NextRequest } from "next/server";
 // EVENT payment_intent.succeeded
 
 export async function POST(req: NextRequest) {
-  console.log("REQ BODY ----> // ", req.body);
+  try {
+    if (!req.body) {
+      return {
+        status: 400,
+        body: { error: "Missing request body" },
+      };
+    }
+    const event = req.body;
+    console.log("Event received:", event);
+
+    return {
+      status: 200,
+    };
+  } catch (error) {
+    console.error("Internal Error:", error);
+    return {
+      status: 500,
+      body: { error: `Internal Server Error: ${error}` },
+    };
+  }
 }
