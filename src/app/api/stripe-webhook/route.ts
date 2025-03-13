3c294be524083e44684d5656d03ae5c41c738e87
Jordi Roca <jordi.roca@acaitravel.com>
2025-03-13 18:14:30 +0100

    fix: webhook test

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -6,6 +6,8 @@ export async function POST(req: NextRequest) {
   // Get the Stripe-Signature header to verify the event
   const sig = req.headers.get("stripe-signature");
 
+  console.log(req.body, "Request body here --------->");
+
   // Read the raw body as text
   const buf = await req.text();
 
@@ -23,9 +25,27 @@ export async function POST(req: NextRequest) {
     return new NextResponse(`Webhook Error: ${err}`, { status: 400 });
   }
 
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    // Option 1: Using customer_email
+    if (session.customer_email) {
+      console.log("Customer Email (customer_email):", session.customer_email);
+    }
+
+    // Option 2: Using customer_details.email
+    if (session.customer_details && session.customer_details.email) {
+      console.log(
+        "Customer Email (customer_details.email):",
+        session.customer_details.email
+      );
+    }
+  }
+
   // Log the event type and object for debugging purposes
   console.log("✅ Success: Event received", event.type);
-  console.log(event.data.object);
+  console.log(event.data.object, "<---- Event data object here");
+  console.log(event, "<---- Event raw info");
 
   // Here you can add additional event handling logic based on event.type
   // For example, if (event.type === 'payment_intent.succeeded') { ... }
