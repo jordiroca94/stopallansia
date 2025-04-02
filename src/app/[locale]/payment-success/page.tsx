2bfca58640d7af273f5615ef07b1a358d92e20cc
Jordi Roca <jordi.roca@acaitravel.com>
2025-04-03 01:29:22 +0200

    fix: confirmation message

--- src/app/[locale]/payment-success/page.tsx
+++ src/app/[locale]/payment-success/page.tsx
@@ -8,10 +8,12 @@ export default function page() {
       <Header />
       <div className="min-h-screen flex flex-col justify-between">
         <Container className="flex-grow flex items-center justify-center py-20">
-          <h1 className="flex justify-center text-lg lg:text-3xl">
-            Payment succesfully sent, an email with the details has been sent to
-            you.
+          <h1 className="flex justify-center text-lg lg:text-2xl">
+            Payment succesfully sent.
           </h1>
+          <p className="flex justify-center text-lg lg:text-2xl">
+            You will receive a confirmation email shortly.
+          </p>
         </Container>
         <Footer />
       </div>
