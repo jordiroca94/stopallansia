3d9bf3036d151f3f90c5fff4808b29c15954de69
Jordi Roca <jordi.roca@acaitravel.com>
2025-04-04 00:29:01 +0200

    fix: description added to route

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -3,11 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const { amount } = await req.json();
+    const { amount, description } = await req.json();
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: "eur",
+      description: description,
       automatic_payment_methods: { enabled: true },
     });
 
