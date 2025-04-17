4823717de228499e061ab705d88b1a277fd0e765
Jordi Roca <jordi.roca@acaitravel.com>
2025-04-17 19:18:52 +0200

    feat: developed by

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaGithub } from "react-icons/fa";
 import Logo from "../icons/footerLogo";
 import { useTranslations } from "next-intl";
 
@@ -19,7 +19,27 @@ const Footer = () => {
           </h5>
           <FaInstagram className="size-6 lg:size-7 transition-transform duration-300 hover:scale-125" />
         </a>
+
         <div>{Logo}</div>
+
+        <div className="flex items-center gap-2 text-base mt-2 ">
+          Developed by
+          <a
+            href="https://jordirocasoler.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:animate-pulse"
+          >
+            Jordi Roca
+          </a>
+          <a
+            href="https://github.com/jordiroca94"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub className="size-5 transition-transform duration-300 hover:scale-125" />
+          </a>
+        </div>
       </div>
     </footer>
   );
